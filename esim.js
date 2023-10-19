82afc0375d6469ff4a02d44bfaf6455ba5b62430
asma0101 <asma.official602@gmail.com>
2023-10-20 00:43:52 +0500

    init

---
+++ esim.js
@@ -0,0 +1,3 @@
+function openUAEPassApp() {
+    window.location.replace('uaepassstg://digitalid-users-ids/signatures/oJXdQOq21Tsx0rttZ4DY?successurl=uaepassEtisalatUAE://&failureurl=uaepassEtisalatUAE://&closeondone=true');
+}
